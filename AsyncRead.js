52520352bfe20dcdc787df24c3774bea7697115d
Joyce <joycesilveiracos@gmail.com>
2021-10-06 14:08:35 -0300

    Módulo para ser lido de forma assíncrona, no Node.js v12

---
+++ AsyncRead.js
@@ -0,0 +1,14 @@
+//  módulo para ser lido de forma assíncrona
+
+const fs = require("fs").promises;
+
+async function read() {
+  const data = await fs.readFile("texto.txt", "binary");
+  return new buffer.from(data);
+}
+
+try {
+  read().then((data) => console.log(data.toString()));
+} catch (error) {
+  console.log(err);
+}
