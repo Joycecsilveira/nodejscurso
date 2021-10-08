06362fce5a782c23b218bb1f4e49a9ac121a0627
Joyce <joycesilveiracos@gmail.com>
2021-10-08 14:56:54 -0300

    modulos

---
+++ modules/events.mjs
@@ -0,0 +1,19 @@
+//Testando módulos natibos
+import { EventEmitter } from "events";
+
+class Evento extends EventEmitter {}
+const meuEvento = new Evento(); //Instância do objeto, copia esse objeto e inicia.
+
+//subscriber - assinante
+meuEvento.on("seguranca", (x, y) => {
+  console.log(`Executando o evento 'seguranca': ${x} ${y}`);
+});
+
+//publisher - emissor
+meuEvento.emit("seguranca", "userAdmin", "Alterou o cargo");
+
+meuEvento.on("Encerrar", (dados) => {
+  console.log("Assinante: " + dados);
+});
+
+meuEvento.emit("Encerrar", "Encerrando a execução da importação dos dados!");
