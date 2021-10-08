f9e53c290ea71d18a767dfed18117a8f933788e1
Joyce <joycesilveiracos@gmail.com>
2021-10-08 14:58:47 -0300

    Compressão de vídeos com FFMPEG e NodeJS

---
+++ videos/cp.js
@@ -0,0 +1,18 @@
+//ChildProcess
+//Execucão de ls (comando em terminal)
+const { spawn } = require("child_process");
+const ls = spawn("ls", ["-lh", "/usr"]);
+
+//Stdio
+ls.stdout.on("data", (data) => {
+  console.log(`stdout: ${data}`);
+});
+ls.stderr.on("data", (data) => {
+  console.log(`stderr: ${data}`);
+});
+
+ls.on("close", (code) => {
+  console.log(
+    `Processo em segundo plano finalizado com sucesso! \n code: ${code}`
+  );
+});
